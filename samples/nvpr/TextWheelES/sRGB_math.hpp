3e1461c47c8e580fc1fe87b2913e5a9a1c7a1e54
Lars Bishop <lbishop@nvidia.com>
2016-06-06 10:53:14 -0400

    Adding redirection link/doc and deleting the files at TOT to indicate that this repo is deprecated and replaced by the new samples tree.

--- samples/nvpr/TextWheelES/sRGB_math.hpp
+++
@@ -1,8 +0,0 @@
-// TAGRELEASE: PUBLIC
-
-// sRGB_math.hpp - sRGB color space conversion utilities
-
-extern float convertLinearColorComponentToSRGBf(const float cl);
-extern unsigned char convertLinearColorComponentToSRGBub(const float cl);
-extern float convertSRGBColorComponentToLinearf(const float cs);
-
--- samples/nvpr/Tiger3DES/glut_teapot.h
+++
@@ -1,9 +0,0 @@
-//----------------------------------------------------------------------------------
-// File:        nvpr/Tiger3DES/glut_teapot.h
-// SDK Version: v2.11 
-// Email:       gameworks@nvidia.com
-// Site:        http://developer.nvidia.com/
-//----------------------------------------------------------------------------------
-// Provide GLUT's teapot API
-extern void glutSolidTeapot(GLdouble scale);
-extern void glutWireTeapot(GLdouble scale);
